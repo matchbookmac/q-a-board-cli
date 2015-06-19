4c400a1ac52a6cb667dc09ba63305e0e9a5f33d2
Ian on MacBook Pro <imacd20@gmail.com>
2015-06-19 12:44:47 -0700

    Display form post form again on deactivating posts page.

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return this.store.find('post');
-  }
+  },
+  showForm: function () {
+    this.get('controller').set('viewPost', true);
+  }.on('deactivate')
 });
